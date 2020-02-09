40ec9b603d9cf8be82a6f2707726fe849aadfcd5
y0620y <nieqiujun@126.com>
2020-02-10 07:32:56 +0800

    连接mongo增加用户名密码

--- bin/main.js
+++ bin/main.js
@@ -1,22 +1,24 @@
-const mongoose=require('mongoose')
-const http=require('http')
+const mongoose = require('mongoose')
+const http = require('http')
 require('../model')
-let app=require('../app')
-let server=http.createServer(app)
+let app = require('../app')
+let server = http.createServer(app)
 
-mongoose.connect("mongodb://127.0.0.1/test",{useMongoClient:true},function (err) {
+
+// 设置用户名 nieqiujun，密码admin123
+mongoose.connect("mongodb://nieqiujun:admin123@39.98.147.33:27017/test", { useMongoClient: true }, function (err) {
     console.log("mongodb 已连接!")
     console.log(err)
-    if(!err){
-        server.listen(3000,function (err) {
-            if(!err){
+    if (!err) {
+        server.listen(3000, function (err) {
+            if (!err) {
                 console.log("express 服务器已打开 ")
             }
         })
     }
 })
 
-server.on('close',function () {
+server.on('close', function () {
     mongoose.disconnect()
 })
 
